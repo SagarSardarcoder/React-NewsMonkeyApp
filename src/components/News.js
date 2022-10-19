10835a3d25bf4b121c587cc09704316ac73aa5e6
sagarsardar coder <sagarsardar.net@gmail.com>
2022-10-19 18:02:17 +0530

    some changes on class based component

--- src/components/News.js
+++ src/components/News.js
@@ -57,8 +57,8 @@ export class News extends Component {
     }
 
     fetchMoreData = async () => {  
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page +1}&pageSize=${this.props.pageSize}`;
         this.setState({page: this.state.page + 1})
-        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json()
         this.setState({
@@ -70,7 +70,7 @@ export class News extends Component {
     render() {
         return (
             <>
-                <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
+                <h1 className="text-center" style={{ margin: '35px 0px', marginTop:"90px" }}>NewsMonkey - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
                 {this.state.loading && <Loading />}
                 <InfiniteScroll
                     dataLength={this.state.articles.length}
